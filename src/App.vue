dedd16f58d6313238c2071972fe1e46f87b09e3e
junwu <junwu2012@hotmail.com>
2020-04-07 21:22:08 +0100

    get data from nationalgrid

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      from: "",
+      to: "",
+      generationmix: []
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  mounted(){
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(response => response.json())
+    .then((result) => {
+      // console.log(result);
+      this.from = result.data.from;
+      this.to = result.data.to;
+      this.generationmix = result.data.generationmix;
+      console.log(this.from);
+      console.log(this.to);
+      console.log(this.generationmix);
+    })
+
+    // .then((result) => {
+    //     const fuelData = map(result.data, 'generationmix');
+
+    //     //Turns our array of arrays of objects into one array of objects
+    //     // e.g. [[{a: 1}, {b: 2}], [{c: 3}, {d: 4}}]] => [{a:1}, {b:2}, {c:3}, {d:4}]
+    //     const flattenedFuelData = flatten(fuelData);
+
+    //     //Groups all our data together as an object by property name
+    //     // e.g. [{a:1}, {b:2}, {a: 3}, {b: 4}] => {a: [{a:1}, {a:3}], b: [{b:2}, {b:4}]}
+    //     const groupedFuelData = groupBy(flattenedFuelData, 'fuel');
+
+    //     const averagedFuelData = map(groupedFuelData, (array, fuelType) => {
+    //       //sumBy allows us to reduce an array by a specific property
+    //       const totalPercentage = sumBy(array, 'perc');
+    //       const averagePercentage = (totalPercentage/array.length).toFixed(2);
+    //       return {'fuel': fuelType, 'perc': parseInt(averagePercentage)}
+    //     })
+
+
   }
 }
 </script>
